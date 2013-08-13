ca9b7bdd6852528a62b4f8835edd4775bc0fa3b6
Brian R. Bondy <netzen@gmail.com>
2013-08-13 19:12:32 -0400

    Issue #55 - Unable to get property 'start_time' of undefined

--- KhanAcademy/pages/videoPage/videoPage.ts
+++ KhanAcademy/pages/videoPage/videoPage.ts
@@ -107,10 +107,10 @@ module VideoPage {
                     //decrement selected index
                     selectedScriptIndex--;
 
-                    if (newTime < script[selectedScriptIndex].start_time) {
+                    if (!script[selectedScriptIndex] || newTime < script[selectedScriptIndex].start_time) {
                         //playhead moved back more than a full segment
                         for (var i = selectedScriptIndex - 1; i > -1; i--) {
-                            if (newTime < script[i].end_time) {
+                            if (!script[i] || newTime < script[i].end_time) {
                                 selectedScriptIndex = i;
                             }
                         }
