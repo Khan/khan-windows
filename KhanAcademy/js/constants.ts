3f67db0f8a86b3b6732b55e646b0e12189ca24d4
Brian R. Bondy <netzen@gmail.com>
2014-03-30 16:27:31 -0400

    Issue #13 - Base implementation for reporting points for watched videos.

--- KhanAcademy/js/constants.ts
+++ KhanAcademy/js/constants.ts
@@ -10,6 +10,7 @@
     export var URL_TOPIC_TREE = URL_API_BASE + "v1/topictree";
     export var URL_USER = URL_API_BASE + "v1/user";
     export var URL_USER_VIDEOS = URL_API_BASE + "v1/user/videos";
+    export var URL_USER_VIDEO_LOG = URL_API_BASE + "v1/user/videos/<youtubeid>/log";
     export var URL_REGISTRATION = URL_BASE + "signup";
 
     export var HTTP_METHOD_GET = "GET";
