2f9eb106632726b3fd63b2b74be9d4ee04b26a29
Brian R. Bondy <netzen@gmail.com>
2014-05-02 14:08:39 -0400

    Issue #108 - use new Windows topic tree KA API endpoint

--- KhanAcademy/js/constants.ts
+++ KhanAcademy/js/constants.ts
@@ -7,7 +7,7 @@
     export var URL_API_BASE = URL_BASE + "api/";
     export var URL_REQUEST_TOKEN = URL_API_BASE + "auth/request_token";
     export var URL_ACCESS_TOKEN = URL_API_BASE + "auth/access_token";
-    export var URL_TOPIC_TREE = URL_API_BASE + "v1/topictree";
+    export var URL_TOPIC_TREE = URL_API_BASE + "v1/win/topictree";
     export var URL_USER = URL_API_BASE + "v1/user";
     export var URL_USER_VIDEOS = URL_API_BASE + "v1/user/videos";
     export var URL_USER_VIDEO_LOG = URL_API_BASE + "v1/user/videos/<youtubeid>/log";
